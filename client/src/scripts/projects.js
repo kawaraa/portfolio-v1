f322b978c0271fe6c6ad828c693e48679df9bc07
Ahmad Kawara <kawaraa4@gmail.com>
2019-03-26 12:17:23 +0100

    Add filter for projects and the social-app project.\

--- client/src/scripts/projects.js
+++ client/src/scripts/projects.js
@@ -1,12 +1,19 @@
-const PROJECTS = [
+const projects = [
   {
-    isAll: true,
     tech: ["javascript", "css", "node.js"],
     link: "https://kawaraa.com"
   },
   {
-    isAll: true,
-    tech: ["javascript", "css", "node.js", "react"],
+    tech: ["javascript", "css"],
     link: "https://social-app.kawaraa.com"
   }
 ];
+
+function filterProjects(query) {
+  if (query === "all") return projects.map(el => el.link);
+  const links = [];
+  projects.forEach(el => {
+    if (el.tech.find(el => el === query)) links.push(el.link);
+  });
+  return links;
+}
